90d12aa4be98d6ba60da252a8335f7e766f8a283
동섭 <sk71927170@gmail.com>
2024-01-05 06:31:08 +0900

    ADD : 사이트 틀제작을 위한 브랜치 생성

--- src/component/Nav/Nav.tsx
+++ src/component/Nav/Nav.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './nav.module.scss';
 
 const Nav = () => {
-  return <div className={classes.wrapNav}>Nav</div>;
+  return <div className={classes.wrapNav}>Nav!</div>;
 };
 
 export default Nav;
